331506f9491e2c057ef3bd35038ca59b8c27f13d
Paul Symons <paul.symons@mechanicalrock.io>
2018-05-08 14:16:27 +0800

    Adding tests and support for externalId usage with STS assumeRole

--- src/IAM.ts
+++ src/IAM.ts
@@ -1,9 +1,6 @@
 
 import {STS, Credentials} from 'aws-sdk'
 
-
-let sts = new STS();
-
 export const roleArn = (params: RoleParams):string => {
     let pathPrefix = params.path || 'role/'
     return `arn:aws:iam::${params.accountId}:${pathPrefix}${params.roleName}`
@@ -13,24 +10,42 @@ interface RoleParams {
     roleName: string,
     accountId: string,
     path?: string,
+    externalId?: string
 }
 
 export class IamRole {
     params;
+    lazySTSClient:STS
 
     constructor(params: RoleParams){
         this.params = params
+    }
+
+    async stsClient() {
+        if (this.lazySTSClient) {
+            return this.lazySTSClient
+        } else {
+            this.lazySTSClient = new STS()
+            return this.lazySTSClient
+        }
     }
 
     toString() {
         return this.params.roleName
     }
 
+    getOptions() : STS.AssumeRoleRequest {
+        let basicOptions = {
+            RoleArn : roleArn(this.params),
+            RoleSessionName : 'BDIComplianceTest',
+            ExternalId : this.params.externalId
+        }        
+        return basicOptions
+    }
+    
     async credentials(): Promise<Credentials> {
-        return sts.assumeRole({
-            RoleArn: roleArn(this.params),
-            RoleSessionName: 'BDIComplianceTest',        
-        }).promise().then(result => {
+        let client = await this.stsClient()
+        return client.assumeRole( this.getOptions() ).promise().then(result => {
             if(!result.Credentials){
                 throw new Error('No credentials provided')
             }
@@ -39,7 +54,7 @@ export class IamRole {
                 secretAccessKey: result.Credentials.SecretAccessKey,
                 sessionToken: result.Credentials.SessionToken,
             })
-        });
+        })
     }
 }
 
@@ -47,21 +62,6 @@ const role = (params: RoleParams): IamRole => {
     return new IamRole(params)
 }
 
-const roleOld = (params: RoleParams):Promise<Credentials> => {
-    return sts.assumeRole({
-        RoleArn: roleArn(params),
-        RoleSessionName: 'BDIComplianceTest',        
-    }).promise().then(result => {
-        if(!result.Credentials){
-            throw new Error('No credentials provided')
-        }
-        return new Credentials({
-            accessKeyId: result.Credentials.AccessKeyId,
-            secretAccessKey: result.Credentials.SecretAccessKey,
-            sessionToken: result.Credentials.SessionToken,
-        })
-    });
-}
 
 export const IAM = {
     role: role,
--- test-data/approval-test-generator.ts
+++ test-data/approval-test-generator.ts
@@ -1,72 +1,90 @@
-import { S3 } from 'aws-sdk'
+import { S3, STS } from 'aws-sdk'
 import fs = require('fs')
 import { IAM } from '../src/IAM'
 
 const approvalTest = async () => {
-    try {
-        let accountId: string = process.env['AWS_ACCOUNT_ID'] || 'unknown'
-        var credentials = await IAM.role({
-            roleName: 'AdminRole',
-            accountId: accountId,
-        }).credentials()
-
-        var s3 = new S3({ credentials: credentials })
-
-        let existingBucketName = 's3001remediate'
-        let nonExistingBucketName = 'thisBucketShouldNotExist'
-
-        s3.headBucket({ Bucket: existingBucketName }).promise().then(writeTo('headBucket-exists.json'))
-        s3.headBucket({ Bucket: nonExistingBucketName }).promise().catch(writeTo('headBucket-notExists.json'))
-        
-        s3.getBucketEncryption({ Bucket: 's3-021-compliant' }).promise().then(writeTo('getBucketEncryption-encrypted.json'))
-        s3.getBucketEncryption({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketEncryption-notExist.json'))
-        
-        s3.listObjects({ Bucket: existingBucketName }).promise().then(writeTo('listObjects-granted.json')).catch(console.log)
-        s3.listObjects({ Bucket: existingBucketName }).promise().catch(writeTo('listObjects-denied.json'))
-        s3.listObjects({ Bucket: nonExistingBucketName }).promise().catch(writeTo('listObjects-notExist.json'))
-        
-        // uncomment desired version (manual setup required)
-        // s3.getBucketLogging({ Bucket: existingBucketName }).promise().then(writeTo('getBucketLogging-enabled.json'))
-        // s3.getBucketLogging({ Bucket: existingBucketName }).promise().then(writeTo('getBucketLogging-disabled.json'))
-        s3.getBucketLogging({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketLogging-notExist.json'))
-        
-        
-        // uncomment desired version (manual setup required)
-        // s3.getBucketVersioning({ Bucket: existingBucketName }).promise().then(writeTo('getBucketVersioning-enabled.json'))
-        // s3.getBucketVersioning({ Bucket: existingBucketName }).promise().then(writeTo('getBucketVersioning-disabled.json'))
-        s3.getBucketVersioning({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketVersioning-notExist.json'))
-        
-        
-        // uncomment desired version (manual setup required)
-        // s3.getBucketAcl({ Bucket: existingBucketName }).promise().then(writeTo('getBucketAcl-publicReadAllUsers.json'))
-        // s3.getBucketAcl({ Bucket: existingBucketName }).promise().then(writeTo('getBucketAcl-private.json'))
-        s3.getBucketAcl({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketAcl-notExist.json'))
-        
-        // uncomment desired version (manual setup required)
-        // s3.getBucketPolicy({ Bucket: existingBucketName }).promise().then(writeTo('getBucketPolicy-denyAnonymous.json'))
-        // s3.getBucketPolicy({ Bucket: existingBucketName }).promise().then(writeTo('getBucketPolicy-public.json'))
-        s3.getBucketPolicy({ Bucket: existingBucketName }).promise().catch(writeTo('getBucketPolicy-none.json'))
-        s3.getBucketPolicy({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketPolicy-notExist.json'))
-
-        let existingFile = 'test/existingFile'
-        let nonExistingFile = 'test/nonExistingFile'
-
-        s3.putObject({Bucket: existingBucketName, Body : 'sample',Key : existingFile}).promise().then(writeTo('putObject-granted.json')).catch(writeTo('putObject-denied.json'))
-        s3.putObject({Bucket: nonExistingBucketName, Body : 'sample',Key : existingFile}).promise().catch(writeTo('putObject-notExist.json'))
-        
-        s3.getObject({Bucket: existingBucketName, Key : existingFile}).promise().then(writeTo('getObject-granted.json')).catch(writeTo('getObject-denied.json'))
-        s3.getObject({Bucket: existingBucketName, Key : nonExistingFile}).promise().then(writeTo('getObject-noSuchKey.json')).catch(writeTo('getObject-errorNoSuchKey.json'))
-        s3.getObject({Bucket: nonExistingBucketName, Key : existingFile}).promise().catch(writeTo('getObject-notExist.json'))
-        
-        s3.deleteObject({Bucket: existingBucketName, Key : existingFile}).promise().then(writeTo('deleteObject-granted.json')).catch(writeTo('deleteObject-denied.json'))
-        s3.deleteObject({Bucket: existingBucketName, Key : nonExistingFile}).promise().then(writeTo('deleteObject-noSuchKey.json')).catch(writeTo('deleteObject-errorNoSuchKey.json'))
-        s3.deleteObject({Bucket: nonExistingBucketName, Key : existingFile}).promise().catch(writeTo('deleteObject-notExist.json'))
-    }
-    catch (err) {
+    try {        
+        // s3Tests( credentials() )
+        stsTests( )
+        stsTests( {externalId: 'Testing'})
+    } catch (err) {
         console.log("Test data generation failed: " + err)
     }
-
 }
+
+const credentials = async (extraParams?) => {
+    let accountId: string = process.env['AWS_ACCOUNT_ID'] || 'unknown'
+    let opts = {
+        roleName: 'Obsidian',
+        accountId: accountId,        
+    }    
+    return  await IAM.role({...opts,...extraParams}).credentials()
+}
+
+
+const s3Tests = (credentials) => {
+    var s3 = new S3({ credentials: credentials })
+
+    let existingBucketName = 's3001remediate'
+    let nonExistingBucketName = 'thisBucketShouldNotExist'
+
+    s3.headBucket({ Bucket: existingBucketName }).promise().then(writeTo('headBucket-exists.json'))
+    s3.headBucket({ Bucket: nonExistingBucketName }).promise().catch(writeTo('headBucket-notExists.json'))
+    
+    s3.getBucketEncryption({ Bucket: 's3-021-compliant' }).promise().then(writeTo('getBucketEncryption-encrypted.json'))
+    s3.getBucketEncryption({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketEncryption-notExist.json'))
+    
+    s3.listObjects({ Bucket: existingBucketName }).promise().then(writeTo('listObjects-granted.json')).catch(console.log)
+    s3.listObjects({ Bucket: existingBucketName }).promise().catch(writeTo('listObjects-denied.json'))
+    s3.listObjects({ Bucket: nonExistingBucketName }).promise().catch(writeTo('listObjects-notExist.json'))
+    
+    // uncomment desired version (manual setup required)
+    // s3.getBucketLogging({ Bucket: existingBucketName }).promise().then(writeTo('getBucketLogging-enabled.json'))
+    // s3.getBucketLogging({ Bucket: existingBucketName }).promise().then(writeTo('getBucketLogging-disabled.json'))
+    s3.getBucketLogging({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketLogging-notExist.json'))
+    
+    
+    // uncomment desired version (manual setup required)
+    // s3.getBucketVersioning({ Bucket: existingBucketName }).promise().then(writeTo('getBucketVersioning-enabled.json'))
+    // s3.getBucketVersioning({ Bucket: existingBucketName }).promise().then(writeTo('getBucketVersioning-disabled.json'))
+    s3.getBucketVersioning({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketVersioning-notExist.json'))
+    
+    
+    // uncomment desired version (manual setup required)
+    // s3.getBucketAcl({ Bucket: existingBucketName }).promise().then(writeTo('getBucketAcl-publicReadAllUsers.json'))
+    // s3.getBucketAcl({ Bucket: existingBucketName }).promise().then(writeTo('getBucketAcl-private.json'))
+    s3.getBucketAcl({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketAcl-notExist.json'))
+    
+    // uncomment desired version (manual setup required)
+    // s3.getBucketPolicy({ Bucket: existingBucketName }).promise().then(writeTo('getBucketPolicy-denyAnonymous.json'))
+    // s3.getBucketPolicy({ Bucket: existingBucketName }).promise().then(writeTo('getBucketPolicy-public.json'))
+    s3.getBucketPolicy({ Bucket: existingBucketName }).promise().catch(writeTo('getBucketPolicy-none.json'))
+    s3.getBucketPolicy({ Bucket: nonExistingBucketName }).promise().catch(writeTo('getBucketPolicy-notExist.json'))
+
+    let existingFile = 'test/existingFile'
+    let nonExistingFile = 'test/nonExistingFile'
+
+    s3.putObject({Bucket: existingBucketName, Body : 'sample',Key : existingFile}).promise().then(writeTo('putObject-granted.json')).catch(writeTo('putObject-denied.json'))
+    s3.putObject({Bucket: nonExistingBucketName, Body : 'sample',Key : existingFile}).promise().catch(writeTo('putObject-notExist.json'))
+    
+    s3.getObject({Bucket: existingBucketName, Key : existingFile}).promise().then(writeTo('getObject-granted.json')).catch(writeTo('getObject-denied.json'))
+    s3.getObject({Bucket: existingBucketName, Key : nonExistingFile}).promise().then(writeTo('getObject-noSuchKey.json')).catch(writeTo('getObject-errorNoSuchKey.json'))
+    s3.getObject({Bucket: nonExistingBucketName, Key : existingFile}).promise().catch(writeTo('getObject-notExist.json'))
+    
+    s3.deleteObject({Bucket: existingBucketName, Key : existingFile}).promise().then(writeTo('deleteObject-granted.json')).catch(writeTo('deleteObject-denied.json'))
+    s3.deleteObject({Bucket: existingBucketName, Key : nonExistingFile}).promise().then(writeTo('deleteObject-noSuchKey.json')).catch(writeTo('deleteObject-errorNoSuchKey.json'))
+    s3.deleteObject({Bucket: nonExistingBucketName, Key : existingFile}).promise().catch(writeTo('deleteObject-notExist.json'))
+}
+
+const stsTests = async (externalId?) => {
+    try {
+        let creds = await credentials(externalId)
+        fs.writeFileSync('test-data/assumeRole-granted.json',JSON.stringify(creds))
+    } catch(e) {
+        fs.writeFileSync('test-data/assumeRole-denied.json',JSON.stringify(e))
+    }
+}
+
 const writeTo = (filename) => {
     return (result) => {
         fs.writeFileSync(`test-data/${filename}`, JSON.stringify(result))
