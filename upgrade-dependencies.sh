7ec28436703b9f539173ee92cab144e9efcc27b0
temyers <tim.myerscough@mechanicalrock.io>
2018-05-12 23:15:27 +0800

    Add sample for auto-submit PR

--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -13,3 +13,6 @@ git commit -m "[CI] - auto-upgrade dependencies"
 git push
 
 # TODO - submit pull request
+
+# GITHUB
+curl -X POST -H "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"title": "CI - Auto Update Dependencies", "head": "update-dependencies", "base": "master", "body": "CI updated dependencies"}' https://api.github.com/repos/MechanicalRock/beady-eye/pulls
