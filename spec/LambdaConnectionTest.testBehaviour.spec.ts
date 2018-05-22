d315c07eefc8d4f393dcc92c22425f86bb3aad7d
Richard VGW <richard.forshaw@vgw.co>
2018-05-22 14:24:39 +0800

    Refactor lambdaconnection tests for simplicity

--- spec/LambdaConnectionTest.testBehaviour.spec.ts
+++ spec/LambdaConnectionTest.testBehaviour.spec.ts
@@ -11,7 +11,26 @@ describe("The Connection-Tester lambda",() => {
             connectionTimeout_ms : 2000
     }
 
-    let withMockedConnectionResult = (result) => {
+    let callbackSuccessWithExpectedResult = (result, done) => {
+        return (err,data) => {
+            expect(err).toBe(null)
+            expect(data).toBeDefined()
+            expect(data.result).toBe(result)
+            done()
+        }
+    }
+
+    let callbackFailedWithExpectedMessage = (msgList, done) => {
+        return (err,data) => {
+            expect(err).toBeDefined()
+            expect(data).toBe(undefined)
+            expect(err.message).toBeDefined()
+            msgList.map(x => expect(err.message).toEqual(expect.stringContaining(x)));
+            done();
+        }
+    }
+
+    let withMockedConnectionResult = (result: string | undefined =undefined) => {
         let fakeConnection = () => {
             let s = net.Socket();
             // Fire the intended result, or allow natural timeout if undefined
@@ -29,78 +48,36 @@ describe("The Connection-Tester lambda",() => {
 
     it("should respond true when a successful connection is made", (done) => {
         withMockedConnectionResult('connect')
-    
-        let callback = (err,data) => {
-            expect(err).toBe(null)
-            expect(data).toBeDefined()
-            expect(data.result).toBe(true)
-            done()
-        }
-         connectionTester.connect(event,context,callback)
+        let callback = callbackSuccessWithExpectedResult(true, done);
+        connectionTester.connect(event,context,callback)
     })
     
     it("should respond false when a connection error occurs", (done) => {
         withMockedConnectionResult('error')
-    
-        let callback = (err,data) => {
-            expect(err).toBeDefined()
-            expect(data).toBe(undefined)
-            expect(err.message).toBeDefined()
-            expect(err.message).toEqual(expect.stringContaining('failed'));
-            done();
-        }
+        let callback = callbackFailedWithExpectedMessage(['failed'], done)
         connectionTester.connect(event,context,callback)
     })
 
     it("should respond false when a connection times out", (done) => {
         withMockedConnectionResult()
-        let callback = (err,data) => {
-            expect(err).toBe(null)
-            expect(data).toBeDefined()
-            expect(data.result).toBe(false)
-            done()
-        }
+        let callback = callbackSuccessWithExpectedResult(false, done);
         connectionTester.connect(event,context,callback)  
     })
     
     it("should respond with handled error when the endpoint parameter is missing", (done) => {
-
-        let callback = (err,data) => {
-            expect(err).toBeDefined()
-            expect(data).toBe(undefined)
-            expect(err.message).toBeDefined()
-            expect(err.message).toEqual(expect.stringContaining('Invalid'));
-            expect(err.message).toEqual(expect.stringContaining('missing'));
-            done();
-        }
+        let callback = callbackFailedWithExpectedMessage(['Invalid', 'missing'], done)
         connectionTester.connect({endpointPort: 1234},context,callback)  
 
     })
 
     it("should respond with handled error when the port parameter is missing", (done) => {
-
-        let callback = (err,data) => {
-            expect(err).toBeDefined()
-            expect(data).toBe(undefined)
-            expect(err.message).toBeDefined()
-            expect(err.message).toEqual(expect.stringContaining('Invalid'));
-            expect(err.message).toEqual(expect.stringContaining('missing'));
-            done();
-        }
+        let callback = callbackFailedWithExpectedMessage(['Invalid', 'missing'], done)
         connectionTester.connect({endpointAddress: "Address"},context,callback)  
 
     })
 
     it("should respond with handled error when the port parameter is invalid", (done) => {
-
-        let callback = (err,data) => {
-            expect(err).toBeDefined()
-            expect(data).toBe(undefined)
-            expect(err.message).toBeDefined()
-            expect(err.message).toEqual(expect.stringContaining('Invalid'));
-            expect(err.message).toEqual(expect.stringContaining('should be an integer'));
-            done();
-        }
+        let callback = callbackFailedWithExpectedMessage(['Invalid', 'should be an integer'], done)
         connectionTester.connect({endpointAddress: "Address", endpointPort: "bad"},context,callback)  
 
     })
