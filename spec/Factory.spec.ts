eb9a8e5703349358f983e6785cead2d4d449962c
Matt Tyler <matt.tyler@mechanicalrock.io>
2018-10-09 11:35:43 +0800

    Add tests

--- spec/Factory.spec.ts
+++ spec/Factory.spec.ts
@@ -1,67 +1,103 @@
-import { S3,  } from 'aws-sdk'
-import { Factory, ServiceFactory } from '../src/Factory'
+import * as AWS from "aws-sdk";
+import { Factory, ServiceFactory } from "../src/Factory";
+import AWSMock = require("aws-sdk-mock");
 
-describe('Service Factory', () => {
-    describe('When I construct a factory', () => {
-        let factory: Factory
-        let serviceFactory: ServiceFactory<S3, S3.ClientConfiguration>
+AWSMock.setSDKInstance(AWS);
+
+describe("Service Factory", () => {
+    describe("When I construct a factory", () => {
+        let factory: Factory;
+        let serviceFactory: ServiceFactory<AWS.S3, AWS.S3.ClientConfiguration>;
 
         beforeEach(() => {
-            factory = new Factory({})
-            expect(factory).toBeDefined()
-        })
+            factory = new Factory({
+                accessKeyId: "origin",
+                secretAccessKey: "originKey",
+                sessionToken: "originToken",
+            });
+            expect(factory).toBeDefined();
+        });
 
-        it('should return an object with a ForService method', () => {
-            expect(factory).toHaveProperty('ForService')
-        })
+        it("should return an object with a ForService method", () => {
+            expect(factory).toHaveProperty("ForService");
+        });
 
-        describe('and construct a service factory', () => {
+        describe("and construct a service factory", () => {
             beforeEach(() => {
-                serviceFactory = factory.ForService(S3)
-                expect(serviceFactory).toBeDefined()
-            })
+                serviceFactory = factory.ForService(AWS.S3);
+                expect(serviceFactory).toBeDefined();
+            });
 
-            it('should return an object with a WithOptions method', () => {
-                expect(serviceFactory).toHaveProperty('WithOptions')
-            })
+            it("should return an object with a WithOptions method", () => {
+                expect(serviceFactory).toHaveProperty("WithOptions");
+            });
 
-            it('should return an object with a WithRoleChain method', () => {
-                expect(serviceFactory).toHaveProperty('WithRoleChain')
-            })
+            it("should return an object with a WithRoleChain method", () => {
+                expect(serviceFactory).toHaveProperty("WithRoleChain");
+            });
 
-            describe('and call WithOptions', () => {
-                let serviceFactoryWithOptions: any
+            describe("and call WithOptions", () => {
+                let serviceFactoryWithOptions: any;
                 beforeEach(() => {
                     serviceFactoryWithOptions = serviceFactory.WithOptions({
-                        region: ""
-                    })
-                })
+                        region: "",
+                    });
+                });
 
-                it('should return an object', () => {
-                    expect(serviceFactoryWithOptions).toBeDefined()
-                })
+                it("should return an object", () => {
+                    expect(serviceFactoryWithOptions).toBeDefined();
+                });
 
-                describe('calling WithOptions again', () => {
-                    it('should return an error', () => {
+                describe("calling WithOptions again", () => {
+                    it("should return an error", () => {
                         expect(() => {
                             (serviceFactoryWithOptions as any).WithOptions({
-                                region: ""
-                            })}
-                        ).toThrow()
-                    })
-                })
-            })
+                                region: "",
+                            });
+                        },
+                        ).toThrow();
+                    });
+                });
+            });
 
-            describe('and calling WithRoleChain', () => {
-                let service: S3
+            describe("and calling WithRoleChain", () => {
+                let service: AWS.S3;
+                let mockFn = jest.fn();
+
                 beforeEach(async (done) => {
+                    mockFn.mockClear();
+                    mockFn = jest.fn((request: AWS.STS.AssumeRoleRequest, callback) => {
+                        callback(null, {
+                            Credentials: {
+                                AccessKeyId: `${request.RoleArn}-1`,
+                                SecretAccessKey: `${request.RoleArn}-1`,
+                                SessionToken: `${request.RoleSessionName}-1`,
+                            },
+                        } as AWS.STS.AssumeRoleResponse);
+                    });
+
+                    AWSMock.mock("STS", "assumeRole", mockFn);
+
                     service = await serviceFactory.WithRoleChain({
-                        RoleArn: "MyArn",
-                        RoleSessionName: "MyRoleSessionName"
-                    })
-                    done()
-                })
-            })
-        })
-    })
-})
+                        RoleArn: "MyRoleArn1",
+                        RoleSessionName: "MyRoleSessionName1",
+                    }, {
+                        RoleArn: "MyRoleArn2",
+                        RoleSessionName: "MyRoleSessionName2",
+                    });
+                    done();
+                });
+
+                it("should call each role in the chain when there are multiple roles", () => {
+                    expect(mockFn).toHaveBeenCalledTimes(2);
+                });
+
+                it("should store the correct credentials in the service", () => {
+                    expect(service.config.accessKeyId).toBe("MyRoleArn2-1");
+                    expect(service.config.secretAccessKey).toBe("MyRoleArn2-1");
+                    expect(service.config.sessionToken).toBe("MyRoleSessionName2-1");
+                });
+            });
+        });
+    });
+});
--- src/Factory.ts
+++ src/Factory.ts
@@ -1,5 +1,7 @@
-import { STS } from "aws-sdk";
-import { ServiceFactory } from "./ServiceFactory";
+/* tslint:disable:max-classes-per-file */
+
+import { Credentials, STS } from "aws-sdk";
+import clonedeep = require("lodash.clonedeep");
 
 /*
 Fluent factory system for instantiating AWS services using role-credential
@@ -27,7 +29,7 @@ const myS3svc = new Factory({ credentials: base })
 export type Ctor<T, U> = new (options: U) => T;
 
 export class Factory implements IForService {
-    constructor(private options: STS.ClientConfiguration) {}
+    constructor(private options: STS.ClientConfiguration) { }
 
     public ForService<T, U>(service: Ctor<T, U>) {
         return new ServiceFactory(this.options, service);
@@ -37,3 +39,57 @@ export class Factory implements IForService {
 interface IForService {
     ForService<T, U>(ctor: Ctor<T, U>);
 }
+
+export class ServiceFactory<T, U> implements IWithOptions<T, U>, IWithRoleChain<T, U> {
+    private options?: U = undefined;
+
+    constructor(
+        private stsOptions: STS.ClientConfiguration,
+        private service: Ctor<T, U>) { }
+
+    public WithOptions(options: U): OmitWithOptions<T, U> {
+        if (this.options !== undefined) {
+            throw new Error("WithOptions has already been called");
+        }
+        this.options = options;
+        return this as OmitWithOptions<T, U>;
+    }
+
+    public async WithRoleChain(...roleRequests: STS.AssumeRoleRequest[]): Promise<T> {
+        const credentials = await roleRequests.reduce((acc: Promise<Credentials>, r: STS.AssumeRoleRequest) => {
+            return acc.then((cred) => {
+                const tmpOptions = Object.assign(clonedeep(this.stsOptions), { cred });
+                return this.Assumer(tmpOptions, r);
+            });
+        }, Promise.resolve(this.stsOptions.credentials));
+        const serviceOptions = Object.assign(clonedeep((this.options || {}) as U), { ...credentials });
+        return new this.service(serviceOptions);
+    }
+
+    private async Assumer(options: STS.ClientConfiguration, request: STS.AssumeRoleRequest)
+    : Promise<Credentials> {
+        const response = new STS(options).assumeRole(request);
+        const token = (await response.promise()).Credentials;
+        if (token === undefined) {
+            throw new Error("Retrieved credentials were returned undefined");
+        } else {
+            return {
+                accessKeyId: token.AccessKeyId,
+                secretAccessKey: token.SecretAccessKey,
+                sessionToken: token.SessionToken,
+            } as Credentials;
+        }
+    }
+}
+
+interface IWithOptions<T, U> {
+    WithOptions(options: U): OmitWithOptions<T, U>;
+}
+
+interface IWithRoleChain<T, U> {
+    WithRoleChain(...roleRequests: STS.AssumeRoleRequest[]): Promise<T>;
+}
+
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export type OmitWithOptions<T, U> = Omit<ServiceFactory<T, U>, "WithOptions">;
