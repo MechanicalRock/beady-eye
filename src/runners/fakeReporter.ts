083e2bec5992812d184ef17de944983de9f5c703
Matt Tyler <matt.tyler@mechanicalrock.io>
2018-11-16 10:51:19 +0800

    Remove deferred pattern in favour of promisifying the callback

--- src/runners/fakeReporter.ts
+++ src/runners/fakeReporter.ts
@@ -1,26 +1,8 @@
-class DeferredPromise {
-    resolve;
-    reject;
-    promise;
-  
-    constructor() {
-      this.promise = new Promise((resolve, reject)=> {
-        this.reject = reject
-        this.resolve = resolve
-      })
-    }
-}
-export const fakeReporter = () => {
-  const deferred = new DeferredPromise()
 
-  const reporter = {
-    jasmineDone: (_, done) => {
-      deferred.resolve()
-    }
-  }
-
-  return {
-    promise: deferred.promise,
-    reporter
-  }
-}
+export const fakeReporter = (callback) => {
+    return {
+        jasmineDone: (_, done) => {
+            callback();
+        },
+    };
+};
