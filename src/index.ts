1f5008594ce364e85e23b30780eaf2c44e20a829
AmitMR <amit.sharma@mechanicalrock.io>
2018-12-12 12:38:40 +0800

    Tests and source code for testing DynamoDb compliance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export { S3 }  from './S3'
-export {IAM} from './IAM'
-export {VPC} from './VPC'
-export {RDS} from './RDS'
+export { S3 } from './S3'
+export { IAM } from './IAM'
+export { VPC } from './VPC'
+export { RDS } from './RDS'
 export { RedshiftCluster } from './Redshift'
-export {CloudFront} from './CloudFront';
+export { CloudFront } from './CloudFront';
+export { DynamoDb } from "./DynamoDb";
 
 // All connection testers
 export * from './ConnectionTesters'
